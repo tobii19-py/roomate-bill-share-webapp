1fd1cabe7d2a0cc012b9e0d46e3ab4d8ba7965e8
tobi <tobibags19@gmail.com>
2023-05-23 15:03:11 -0500

    Write empty classes

--- frontend.py
+++ frontend.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask.views import MethodView
+from wtforms import Form
+
+class HomePage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
